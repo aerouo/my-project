1d92aa743ff86743db11d4b4460cc2516cbd834d
YaFAn0512 <yoyotw842@gmail.com>
2026-05-15 16:10:42 +0800

    更新questmap

--- Assets/questmap/Scripts/MapManager.cs
+++ Assets/questmap/Scripts/MapManager.cs
@@ -1,14 +1,60 @@
 using UnityEngine;
+using TMPro;
 using UnityEngine.SceneManagement;
 
 public class MapManager : MonoBehaviour
 {
-    public void OnClickLevel(string sceneName)
+    [Header("UI 面板連結")]
+    public GameObject levelInfoPanel;
+    public TextMeshProUGUI titleText;
+    public TextMeshProUGUI descText;
+
+    private int currentSelectedLevel = 1;
+
+    public void OnClickLevel(int levelNum)
     {
-        SceneManager.LoadScene(sceneName);
+        currentSelectedLevel = levelNum;
+        levelInfoPanel.SetActive(true);
+
+        if (levelNum == 1)
+        {
+            titleText.text = "第一關";
+            descText.text = "你在一個陌生的環境醒來，求生的本能使你想辦法逃出去，但卻不是很順利...";
+        }
+        else if (levelNum == 2)
+        {
+            titleText.text = "第二關";
+            descText.text = "第一次的失敗後，你有點挫折，在思緒中就聽到一些聲響，驚恐的你發現...";
+        }
+        else if (levelNum == 3)
+        {
+            titleText.text = "第三關";
+            descText.text = "你被帶到安全的地方，放鬆警惕的你疲憊不堪，填飽肚子後你又決定繼續尋找出路...";
+        }
+        else if (levelNum == 4)
+        {
+            titleText.text = "第四關";
+            descText.text = "你大致熟悉了這個地方，但本能驅使你仍想趕快逃離，你盤算著如何離開島嶼...";
+        }
+        else if (levelNum == 5)
+        {
+            titleText.text = "第五關";
+            descText.text = "天氣晴朗，你的心情和天氣一樣好，未知的旅程混雜著不安，你還是勇敢踏出了這一步...";
+        }
     }
+
+    public void OnClickStart()
+    {
+        SceneManager.LoadScene("Level" + currentSelectedLevel);
+    }
+
     public void OnClickReturn()
     {
-        SceneManager.LoadScene("home"); // 改成你們大廳的 Scene 名稱
+        SceneManager.LoadScene("home");
+    }
+
+    public void OnClickback()
+    {
+        levelInfoPanel.SetActive(false);
     }
 }
