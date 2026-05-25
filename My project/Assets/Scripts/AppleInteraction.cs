a97ddfe0c170ec8bdcac0f644278f5293413348b
aerouo <a0906936189@gmail.com>
2026-05-26 01:22:11 +0800

    小修

--- My project/Assets/Scripts/AppleInteraction.cs
+++ My project/Assets/Scripts/AppleInteraction.cs
@@ -1,12 +1,12 @@
 ﻿using UnityEngine;
 
-public class AppleInteraction : MonoBehaviour
+public class PuaperInteraction : MonoBehaviour
 {
     public GameObject puaperPanel;   // 拖入 puaper
     public GameObject printGroupPanel; // 拖入 print group
 
     // 點便條紙 → 開啟拖曳視窗
-    public void OnClickApple()
+    public void OnClickPuaper()
     {
         if (puaperPanel != null) puaperPanel.SetActive(false);
         if (printGroupPanel != null) printGroupPanel.SetActive(true);
